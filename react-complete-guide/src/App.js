8f70667fa932c40fbf3caafc603c57783b938ee2
Danny <d.v.groves@hotmail.co.uk>
2019-11-18 15:39:10 +0000

    passing method between components (click)

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -24,9 +24,16 @@ class App extends Component {
       <div className="App">
         <h1>Hi I'm react app</h1>
         <button onClick={this.changeNameHandler}>Change Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+         name={this.state.persons[1].name} 
+         age={this.state.persons[1].age}
+         click={this.changeNameHandler}/>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
     );
   }
