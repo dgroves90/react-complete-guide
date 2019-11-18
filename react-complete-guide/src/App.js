20cd7516a7750ffedea05c6fb8f51726b55e8a5c
Danny <d.v.groves@hotmail.co.uk>
2019-11-18 15:45:22 +0000

    passing arguments in components using bind

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -11,9 +11,9 @@ class App extends Component {
     ]
   }
 
-  changeNameHandler = () => {
+  changeNameHandler = (newName) => {
     this.setState({persons: [
-      {name: "Daniel", age:30},
+      {name: newName, age:30},
       {name: "Sanjib", age:32},
       {name: "Leo", age:23}
     ]})
@@ -23,14 +23,14 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi I'm react app</h1>
-        <button onClick={this.changeNameHandler}>Change Name</button>
+        <button onClick={this.changeNameHandler.bind(this, 'Mr Button')}>Change Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}/>
         <Person 
          name={this.state.persons[1].name} 
          age={this.state.persons[1].age}
-         click={this.changeNameHandler}/>
+         click={this.changeNameHandler.bind(this, 'Mr Paragraph')}/>
         <Person 
           name={this.state.persons[2].name} 
           age={this.state.persons[2].age}/>
